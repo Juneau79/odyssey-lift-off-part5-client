8a178f163e5b3bef8e7c2c128a81bace544070cc
Wilder, Dave <daviwild@amazon.com>
2021-11-03 13:51:54 -0500

    updated uri

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import Pages from './pages';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000',
+  uri: 'https://juneau79-servers-catstronaught.herokuapp.com/',
   cache: new InMemoryCache(),
 });
 
